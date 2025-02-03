75a1b108e7eff3531ae29b32f46954b744cb1b18
Abdullah <abdullahrehan2010@gmail.com>
2025-02-03 18:10:57 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,10 @@
+def power_set():
+
+
+
+    size = int(input("Please Enter array size =  "))
+
+    for i in range(size):
+        element1 =int(input("pls enter element = "))
+
+power_set()
---
+++ power.py
@@ -0,0 +1,27 @@
+
+import math
+def printPowerSet():
+
+    PowerSetSize = (int)(math.pow(2,SetSize));
+    outer = 0;
+    inner = 0;
+    for outer in range(0, PowerSetSize): 
+        for inner in range(0, SetSize):
+
+            if((outer & (1 << inner)) > 0):
+                print(set[inner], end = "")
+
+        print ("'")
+
+
+
+size = int(input("Please Enter array size =  "))
+
+set = []
+for i in range(0,size):
+    n =int(input("pls enter element = "))
+    set.append(n)
+
+printPowerSet(set,len(set))
+
+
